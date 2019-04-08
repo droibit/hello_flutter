1a9ef5a8b8880d929fb37e25ca4a4cc341391449
Shinya Kumagai <roomful.rooms@gmail.com>
2019-04-09 00:07:19 +0900

    Implemented "Introduction to widgets"

--- lib/main.dart
+++ lib/main.dart
@@ -3,5 +3,11 @@ import 'src/introduction_to_widgets.dart';
 
 void main() => runApp(MaterialApp(
   title: 'My app',
-  home: TutorialHome(),
+  home: ShoppingList(
+    products: <Product>[
+      Product(name: 'Eggs'),
+      Product(name: 'Flour'),
+      Product(name: 'Chocolate chips'),
+    ],
+  ),
 ));
--- lib/src/introduction_to_widgets.dart
+++ lib/src/introduction_to_widgets.dart
@@ -34,6 +34,7 @@ class _MyAppBar extends StatelessWidget {
   }
 }
 
+// ref. https://flutter.dev/docs/development/ui/widgets-intro#basic-widgets
 class MyScaffold extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -57,6 +58,7 @@ class MyScaffold extends StatelessWidget {
   }
 }
 
+// ref. https://flutter.dev/docs/development/ui/widgets-intro#basic-widgets
 class TutorialHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -88,6 +90,7 @@ class TutorialHome extends StatelessWidget {
   }
 }
 
+// ref. https://flutter.dev/docs/development/ui/widgets-intro#basic-widgets
 class MyButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -108,5 +111,167 @@ class MyButton extends StatelessWidget {
         ),
       ),
     );
+  }
+}
+
+// ref. https://flutter.dev/docs/development/ui/widgets-intro#basic-widgets
+class Counter extends StatefulWidget {
+  @override
+  State createState() => _CounterState();
+}
+
+class _CounterState extends State<Counter> {
+  int _counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+//        RaisedButton(
+//          onPressed: _increment,
+//          child: Text('increment'),
+//        ),
+//        Text('Count: $_counter'),
+        _CounterIncrementor(onPressed: _increment),
+        _CounterDisplay(count: _counter),
+      ],
+    );
+  }
+
+  void _increment() {
+    setState(() {
+      _counter++;
+    });
+  }
+}
+
+class _CounterDisplay extends StatelessWidget {
+  final int count;
+
+  _CounterDisplay({@required this.count});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text('Count: $count');
+  }
+}
+
+class _CounterIncrementor extends StatelessWidget {
+  final VoidCallback onPressed;
+
+  _CounterIncrementor({@required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return RaisedButton(
+      onPressed: this.onPressed,
+      child: Text('Increment'),
+    );
+  }
+}
+
+class CounterHome extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: Counter(),
+      ),
+    );
+  }
+}
+
+// ref. https://flutter.dev/docs/development/ui/widgets-intro#bringing-it-all-together
+class Product {
+  final String name;
+
+  const Product({@required this.name});
+}
+
+typedef void CartChangedCallback(Product product, bool inCart);
+
+class ShoppingListItem extends StatelessWidget {
+  final Product product;
+  final bool inCart;
+  final CartChangedCallback onChartChange;
+
+  ShoppingListItem({
+    @required product,
+    @required this.inCart,
+    @required this.onChartChange,
+  })  : product = product,
+        super(key: ObjectKey(product));
+
+  Color _getColor(BuildContext context) {
+    return inCart ? Colors.black54 : Theme.of(context).primaryColor;
+  }
+
+  TextStyle _getTextStyle(BuildContext context) {
+    if (!inCart) return null;
+    return TextStyle(
+      color: Colors.black54,
+      decoration: TextDecoration.lineThrough,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      onTap: () {
+        onChartChange(product, !inCart);
+      },
+      leading: CircleAvatar(
+        backgroundColor: _getColor(context),
+        child: Text(product.name[0]),
+      ),
+      title: Text(
+        product.name,
+        style: _getTextStyle(context),
+      ),
+    );
+  }
+}
+
+class ShoppingList extends StatefulWidget {
+  final List<Product> products;
+
+  ShoppingList({Key key, this.products}) : super(key: key);
+
+  @override
+  State createState() => _ShoppingListState();
+}
+
+class _ShoppingListState extends State<ShoppingList> {
+  Set<Product> _shoppingCart = Set();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Shopping List'),
+      ),
+      body: ListView(
+        padding: const EdgeInsets.symmetric(vertical: 8.0),
+        children: widget.products.map((product) {
+          return ShoppingListItem(
+            product: product,
+            inCart: _shoppingCart.contains(product),
+            onChartChange: _handleCartChanged,
+          );
+        }).toList(),
+      ),
+    );
+  }
+
+  void _handleCartChanged(Product product, bool inCart) {
+    setState(() {
+      if (inCart) {
+        _shoppingCart.add(product);
+      } else {
+        _shoppingCart.remove(product);
+      }
+    });
   }
 }
